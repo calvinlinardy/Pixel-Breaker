694894c439e07bd22f22b59602cfd821362ef211
calvinlinardy <calvin.linardy@yahoo.com>
2020-07-28 20:52:40 +0800

    Adding autoplay

--- Assets/Scripts/GameStatus.cs
+++ Assets/Scripts/GameStatus.cs
@@ -9,6 +9,7 @@ public class GameStatus : MonoBehaviour
     [Range(0.1f, 10f)] [SerializeField] float gameSpeed = 1f;
     [SerializeField] int pointsPerBlockDestroyed = 50;
     [SerializeField] TextMeshProUGUI scoreText = null;
+    [SerializeField] bool isAutoPlayEnabled;
 
     //state
     [SerializeField] int currentScore = 0;
@@ -48,5 +49,9 @@ public class GameStatus : MonoBehaviour
         currentScore += pointsPerBlockDestroyed;
         scoreText.text = currentScore.ToString();
     }
-
+    
+    public bool IsAutoPlayEnabled()
+    {
+        return isAutoPlayEnabled;
+    }
 }
--- Assets/Scripts/Paddle.cs
+++ Assets/Scripts/Paddle.cs
@@ -9,18 +9,34 @@ public class Paddle : MonoBehaviour
     [SerializeField] float minX = 1.23f;
     [SerializeField] float maxX = 14.77f;
 
+    // cached references
+    GameStatus theGameStatus;
+    Ball theBall;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        theGameStatus = FindObjectOfType<GameStatus>();
+        theBall = FindObjectOfType<Ball>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        float mousePosInUnits = (Input.mousePosition.x / Screen.width * screenWidthUnits);
         Vector2 paddlePos = new Vector2(transform.position.x, transform.position.y);
-        paddlePos.x = Mathf.Clamp(mousePosInUnits, minX, maxX);
+        paddlePos.x = Mathf.Clamp(GetXPos(), minX, maxX);
         transform.position = paddlePos;
+    }
+
+    private float GetXPos()
+    {
+        if (theGameStatus.IsAutoPlayEnabled())
+        {
+            return theBall.transform.position.x;
+        }
+        else
+        {
+            return Input.mousePosition.x / Screen.width * screenWidthUnits;
+        }
     }
 }
